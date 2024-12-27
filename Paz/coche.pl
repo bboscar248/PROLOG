261d4c54de79e47e488adf325abce28428b2c090
lucia-wu <chenhui.lucia.wu@estudiantat.upc.edu>
2024-12-27 19:39:36 +0100

    Update coche.pl

--- Paz/coche.pl
+++ Paz/coche.pl
@@ -1,7 +1,3 @@
-:- discontiguous averia/2.
-:- discontiguous causa/2.
-:- discontiguous relacion/2.
-
 % Domini dels cotxes
 % Tenim 5 subsistemes: 
 % direccio, transmissio, encesa, sistema elèctric, sistema de confort interior
@@ -13,11 +9,13 @@ subsistema(coche, encesa).
 subsistema(coche, sistema_electrico).
 subsistema(coche, sistema_confort_interior).
 
+
 % Funcionamento normal del sistema
 % Relacion entre componentes y comportamientos esperados
 
 % Direccion
 % Avarias/efectos/observaciones del subsistema de direccion
+
 averia(direccion, vibraciones_volante).
 averia(direccion, ruido_al_girar).
 averia(direccion, direccion_dura).
@@ -45,7 +43,7 @@ causa(direccion, rotura_de_piezas).
 % Reglas que relacionan causas y efectos del subsistema de direccion
 relacion(falta_de_liquido_de_direccion, [direccion_deficiente, direccion_dura]). 
 relacion(fallos_en_la_bomba_de_direccion_asistida, [direccion_deficiente, direccion_dura]).
-relacion(ruedas_mal_alineadas, [direccion_deficiente, vibraciones_volante, desviacion_hacia_un_lado]).
+relacion(ruedas_mal_alineadas, [direccion, deficiente, vibraciones_volante, desviacion_hacia_un_lado]).
 relacion(fallo_en_las_rotulas, [direccion_deficiente, ruido_al_girar]).
 relacion(fallo_en_los_bujes, [direccion_deficiente, ruido_al_girar]).
 relacion(desgastos_en_los_brazos_de_control, [direccion_deficiente]).
@@ -60,76 +58,99 @@ relacion(problemas_de_presion_neumaticos, [desviacion_hacia_un_lado]).
 relacion(problemas_barra_de_direccion, [desviacion_hacia_un_lado, coche_no_gira]).
 relacion(rotura_de_piezas, [coche_no_gira]).
 
+
+
 % Transmissio
-% Avarias/efectos/observaciones del subsistema de transmision
+
+% Avarias/efectos/observaciones del subsistema de transmisión
 averia(transmission, dificultad_para_cambiar_marchas).
 averia(transmission, perdida_de_potencia).
-averia(transmission, ruidos_extranos_en_la_caja).
-averia(transmission, vibraciones_en_el_piso).
+averia(transmission, ruidos_extraños_en_la_caja).
+averia(transmission, vibraciones_al_conducir).
 averia(transmission, sobrecalentamiento).
 
-% Causas posibles de fallos del coche del subsistema de transmision
+% Causas posibles de fallos del coche del subsistema de transmisión
 causa(transmission, nivel_bajo_de_aceite_transmision).
 causa(transmission, desgaste_sincronizadores).
 causa(transmission, embrague_desgastado).
 causa(transmission, filtro_obstruido).
 causa(transmission, fallo_en_la_bomba_de_aceite).
+causa(transmission, convertidor_de_par_defectuoso).
+causa(transmission, engranajes_desgastados).
 causa(transmission, soportes_del_motor_desgastados).
 
-% Reglas que relacionan causas y efectos del subsistema de transmision
+% Reglas que relacionan causas y efectos del subsistema de transmisión
 relacion(nivel_bajo_de_aceite_transmision, [dificultad_para_cambiar_marchas, sobrecalentamiento]).
 relacion(desgaste_sincronizadores, [dificultad_para_cambiar_marchas]).
-relacion(embrague_desgastado, [perdida_de_potencia, dificultad_para_cambiar_marchas]).
+relacion(embrague_desgastado, [perdida_de_potencia]).
 relacion(filtro_obstruido, [sobrecalentamiento]).
-relacion(fallo_en_la_bomba_de_aceite, [ruidos_extranos_en_la_caja]).
-relacion(soportes_del_motor_desgastados, [vibraciones_en_el_piso]).
+relacion(fallo_en_la_bomba_de_aceite, [ruidos_extraños_en_la_caja]).
+relacion(convertidor_de_par_defectuoso, [perdida_de_potencia]).
+relacion(engranajes_desgastados, [ruidos_extraños_en_la_caja]).
+relacion(soportes_del_motor_desgastados, [vibraciones_al_conducir]).
+
+
+
 
 % Encesa
+
 % Avarias/efectos/observaciones del subsistema de encendido
 averia(encesa, el_motor_no_arranca).
-averia(encesa, ralenti_irregular).
+averia(encesa, ralentí_irregular).
 averia(encesa, consumo_excesivo_combustible).
 averia(encesa, detonaciones).
 averia(encesa, perdida_de_potencia).
+averia(encesa, dificultad_arranque_en_frio).
+averia(encesa, chispa_debil).
 
 % Causas posibles de fallos del coche del subsistema de encendido
 causa(encesa, bujias_desgastadas).
 causa(encesa, cables_de_bujias_danados).
 causa(encesa, bobina_de_encendido_fallida).
-causa(encesa, fallo_del_sensor_de_ciguenal).
+causa(encesa, fallo_del_sensor_de_cigueñal).
 causa(encesa, distribuidor_defectuoso).
 causa(encesa, mezcla_combustible_pobre).
+causa(encesa, bateria_descargada).
+causa(encesa, regulador_de_voltaje_defectuoso).
 
 % Reglas que relacionan causas y efectos del subsistema de encendido
-relacion(bujias_desgastadas, [el_motor_no_arranca, ralenti_irregular]).
-relacion(cables_de_bujias_danados, [ralenti_irregular, perdida_de_potencia]).
-relacion(bobina_de_encendido_fallida, [el_motor_no_arranca, detonaciones]).
-relacion(fallo_del_sensor_de_ciguenal, [el_motor_no_arranca]).
-relacion(distribuidor_defectuoso, [detonaciones, ralenti_irregular]).
+relacion(bujias_desgastadas, [el_motor_no_arranca, ralentí_irregular, chispa_debil]).
+relacion(cables_de_bujias_danados, [ralentí_irregular, perdida_de_potencia]).
+relacion(bobina_de_encendido_fallida, [el_motor_no_arranca, detonaciones, chispa_debil]).
+relacion(fallo_del_sensor_de_cigueñal, [el_motor_no_arranca]).
+relacion(distribuidor_defectuoso, [detonaciones, ralentí_irregular]).
 relacion(mezcla_combustible_pobre, [consumo_excesivo_combustible, perdida_de_potencia]).
-relacion(bujias_desgastadas, [consumo_excesivo_combustible]).
+relacion(bateria_descargada, [dificultad_arranque_en_frio]).
+relacion(regulador_de_voltaje_defectuoso, [chispa_debil]).
 
 % Sistema elèctric
-% Avarias/efectos/observaciones del subsistema electrico
+% Avarias/efectos/observaciones del subsistema eléctrico
 averia(sistema_electrico, luces_no_funcionan).
 averia(sistema_electrico, bateria_descargada).
 averia(sistema_electrico, alternador_fallido).
 averia(sistema_electrico, cortocircuitos).
 averia(sistema_electrico, fusibles_fundidos).
+averia(sistema_electrico, sistema_no_responde).
+averia(sistema_electrico, sobrecalentamiento_cables).
 
-% Causas posibles de fallos del coche del subsistema electrico
+% Causas posibles de fallos del coche del subsistema eléctrico
 causa(sistema_electrico, cableado_danado).
 causa(sistema_electrico, bateria_anticuada).
 causa(sistema_electrico, regulador_de_voltaje_defectuoso).
 causa(sistema_electrico, conexiones_sueltas).
-causa(sistema_electrico, alternador_danado).
+causa(sistema_electrico, alternador_dañado).
+causa(sistema_electrico, cortocircuito).
 
-% Reglas que relacionan causas y efectos del subsistema electrico
-relacion(cableado_danado, [cortocircuitos, luces_no_funcionan]).
-relacion(bateria_anticuada, [bateria_descargada]).
-relacion(regulador_de_voltaje_defectuoso, [alternador_fallido]).
+% Reglas que relacionan causas y efectos del subsistema eléctrico
+relacion(cableado_danado, [cortocircuitos, luces_no_funcionan, sobrecalentamiento_cables]).
+relacion(bateria_anticuada, [bateria_descargada, sistema_no_responde]).
+relacion(regulador_de_voltaje_defectuoso, [alternador_fallido, sistema_no_responde]).
 relacion(conexiones_sueltas, [fusibles_fundidos]).
-relacion(alternador_danado, [bateria_descargada, alternador_fallido]).
+relacion(alternador_dañado, [bateria_descargada, alternador_fallido]).
+relacion(cortocircuito, [cortocircuitos, sobrecalentamiento_cables]).
+
+
+
 
 % Sistema de confort interior
 % Avarias/efectos/observaciones del subsistema de confort interior
