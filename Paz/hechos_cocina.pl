023012112d371038ce76bc67318c4656e1cfe5e5
lucia-wu <chenhui.lucia.wu@estudiantat.upc.edu>
2024-12-29 13:30:50 +0100

    Create hechos_cocina.pl

---
+++ Paz/hechos_cocina.pl
@@ -0,0 +1,13 @@
+% Hechos observados
+:- dynamic respuesta/1.
+
+% Respuestas observadas
+respuesta(no_calienta).
+respuesta(no(no_encieden)).
+respuesta(no(hace_ruido)).
+respuesta(no(fuga_de_gas)).
+respuesta(no(no_enfria)).
+respuesta(no(fuga_de_agua)).
+respuesta(no(llama_baja)).
+respuesta(no(calienta_demasiado)).
+respuesta(no(hace_ruido)).
