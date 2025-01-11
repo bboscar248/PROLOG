ecddeb1efb09c4f654ed30563cc3ff8cbdb23dc4
bboscar248 <oscar.lu@estudiantat.upc.edu>
2025-01-12 00:06:08 +0100

    changes

--- Entrada_Sortida/format.pl
+++
@@ -1,5 +0,0 @@
-formatNumeros(X) :-
-    format('Número sense decimals: ~0f~n', [X]), 
-    format('Número amb decimals (1 xifra): ~1f~n', [X]),    
-    format('Número amb decimals (2 xifres): ~2f~n', [X]), 
-    format('Número amb decimals (3 xifres): ~3f~n', [X]).  
